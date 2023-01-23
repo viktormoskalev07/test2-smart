09186770d5b4bc8ccb355d6db854c828f065e0c6
Viktor <80700897+viktormoskalev07@users.noreply.github.com>
2023-01-23 12:10:01 +0200

    e

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import clsx from "clsx";
+import React from "react";
 
-import styles from './home.module.scss'
+import styles from "./home.module.scss";
 
 export const Home = () => {
+  const sides = Array(6).fill(" ");
+  console.log(sides, 2);
 
-	return <div className={styles.home}>
-			<h1>home</h1>
-	</div>
+  return (
+    <div className={styles.home}>
+      <h1>home</h1>
+      <div className={styles.cube}>
+        {sides.map((_, i) => {
+          return (
+            <div key={i} className={clsx("side", "side" + (i + 1))}>
+              {i}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
- 
